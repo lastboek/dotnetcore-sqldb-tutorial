88f23f5e263d4687d2c4a3ebb04bc6614b6b572f
lastboek <lastboek@gmail.com>
2021-10-13 22:23:04 +0000

    Changes for todo app

--- Startup.cs
+++ Startup.cs
@@ -29,9 +29,14 @@ namespace DotNetCoreSqlDb
         {
             services.AddControllersWithViews();
             services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
-        }
+                    options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+            // The following line enables Application Insights telemetry collection.
+            services.AddApplicationInsightsTelemetry();
 
+            // This code adds other services for your application.
+            services.AddMvc();
+        }
+       
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
